dad9aad0dcf35f3644998761b9d26df1b768bfac
Guillaume Delannoy <guillaume@telescop.com>
2023-03-27 16:49:34 +0000

    React 17.

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import {
 export default class App {
   constructor(context) {
     if (!context) {
-      const message = `Context is required when creating a new app.`;
+      const message = 'Context is required when creating a new app.';
       throw new Error(message);
     }
 
@@ -37,18 +37,18 @@ export default class App {
     this._checkForInit();
 
     if (!module) {
-      const message = `Should provide a module to load.`;
+      const message = 'Should provide a module to load.';
       throw new Error(message);
     }
 
     if (module.__loaded) {
-      const message = `This module is already loaded.`;
+      const message = 'This module is already loaded.';
       throw new Error(message);
     }
 
     if (module.routes) {
       if (typeof module.routes !== 'function') {
-        const message = `Module's routes field should be a function.`;
+        const message = 'Module\'s routes field should be a function.';
         throw new Error(message);
       }
 
@@ -63,7 +63,7 @@ export default class App {
 
     if (module.load) {
       if (typeof module.load !== 'function') {
-        const message = `module.load should be a function`;
+        const message = 'module.load should be a function';
         throw new Error(message);
       }
 
@@ -92,7 +92,7 @@ export default class App {
 
   _checkForInit() {
     if (this.__initialized) {
-      const message = `App is already initialized`;
+      const message = 'App is already initialized';
       throw new Error(message);
     }
   }
